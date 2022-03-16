bc1a09285cdd101718f44bafea9411a377b72ac1
Abdullah-alnoumas <70814254+Abdullah-alnoumas@users.noreply.github.com>
2022-03-16 19:46:55 +0300

    done

--- d5-cw3.playground/Contents.swift
+++ d5-cw3.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+func printUserName(){
+    print("AbdullahNoumas")
+}
+
+func Grade(first:Int,second:Int,third:Int)->String{
+   
+    let GradeAverage = (first + second + third) / 3
+  
+    if ( GradeAverage >= 90 && GradeAverage <= 100){
+           return "A"
+       }
+       else if ( GradeAverage >= 80 && GradeAverage <= 89 ){
+           return "B"
+       }
+       else if ( GradeAverage >= 70 && GradeAverage <= 79 ){
+           return "C"
+       }
+       else if ( GradeAverage >= 60 && GradeAverage <= 69 ){
+           return "D"
+       }
+       else{
+           return "F"
+           }
+   
+
+}
+
+print(Grade(first:78, second:89 , third:98))
+
+
